a2c16cd071ab38ee19932969b19d73b9579b62e0
Chandra Yadav <chandra.yadav@maplewave.com>
2025-02-24 23:44:13 -0400

    Updated env file location- CY

--- src/api/CatApi.ts
+++ src/api/CatApi.ts
@@ -1,6 +1,7 @@
 import { CatData } from "../interfaces/ICatData";
 
 const API_URL = process.env.REACT_APP_API_URL;
+console.log("Backend API URL: ", API_URL);
 
 export const fetchCatData = async (): Promise<CatData> => {
   const response = await fetch('https://localhost:44344/api/cat');
