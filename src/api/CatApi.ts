ff80f085178246eaa2beb3ea87697d6e15db9980
Chandra Yadav <chandra.yadav@maplewave.com>
2025-02-25 00:21:13 -0400

    Updated azure api URL- CY

--- src/api/CatApi.ts
+++ src/api/CatApi.ts
@@ -4,8 +4,8 @@ const API_URL = process.env.REACT_APP_API_URL;
 console.log("Backend API URL: ", API_URL);
 
 export const fetchCatData = async (): Promise<CatData> => {
-  const response = await fetch('https://localhost:44344/api/cat');
-  //const response = await fetch(API_URL as string);
+  //const response = await fetch('https://localhost:44344/api/cat');
+  const response = await fetch(API_URL as string);
   if (!response.ok) {
     throw new Error('Network response was not ok');
   }
